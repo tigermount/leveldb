cc6c2b938c4ec8cd0710a54e3dd4d4cd5d29c70a
tigermount <patrsun@amazon.com>
2020-12-01 23:34:04 -0500

    Adding test

---
+++ app/app_test.cc
@@ -0,0 +1,30 @@
+//
+// Created by Sun, Patrick on 12/1/20.
+//
+
+#include "app_test.h"
+#include "leveldb/db.h"
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+using namespace leveldb;
+
+int main() {
+    leveldb::DB *db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, "testdb", &db);
+    assert(status.ok());
+
+    status = db->Put(WriteOptions(), "KeyNameExample", "ValueExample");
+    assert(status.ok());
+    cout << status.ToString() << endl;
+    string res;
+    status = db->Get(ReadOptions(), "KeyNameExample", &res);
+    assert(status.ok());
+    cout << res << endl;
+
+    delete db;
+    return 0;
+}
---
+++ app/app_test.h
@@ -0,0 +1,8 @@
+//
+// Created by Sun, Patrick on 12/1/20.
+//
+
+#ifndef LEVELDB_APP_TEST_H
+#define LEVELDB_APP_TEST_H
+
+#endif //LEVELDB_APP_TEST_H
